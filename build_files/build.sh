f8700bb207a1fb2ad96b983762baba6d8d329e4a
SED4906 <sed4906birdie@gmail.com>
2026-08-30 13:18:20 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -90,8 +90,6 @@ systemctl --global add-wants niri.service waybar.service
 systemctl --global add-wants niri.service swaybg.service
 systemctl --global add-wants niri.service swayidle.service
 
-systemctl set-default graphical.target
-
 dnf5 -y install pcsc-lite
 
 # Copy Files to Container
